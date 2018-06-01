7fc196464ed876adc600675a3c06bb53cb7d2f9b
Sonia Wadji <soniawadji@gmail.com>
2018-06-02 00:40:31 +0530

    Commit 1.3 Completed Recycler View :) with dummy data

--- app/src/main/java/newsapp/sonia/com/cfnewsapp/MainActivity.kt
+++ app/src/main/java/newsapp/sonia/com/cfnewsapp/MainActivity.kt
@@ -2,11 +2,49 @@ package newsapp.sonia.com.cfnewsapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var newsList: ArrayList<News> = ArrayList()
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var linearLayoutManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setNewsList()
+
+        newsAdapter = NewsAdapter(context = this, newsList = newsList)
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
+        recyclerView.adapter = newsAdapter
+
+    }
+
+    private fun setNewsList() {
+        var news = News("JHbdajh", "kajbscjinascakjsbcahbsc", "sjdhcjsd",
+                "skjdhcljsdn")
+        newsList.add(news)
+
+        news = News("qbwjkd", "oiu92yewdiquhwl", "sjdhcjsd",
+                "skjdhcljsdn")
+        newsList.add(news)
+
+        news = News("wkjsdnda", "123rd2we", "sjdhcjsd",
+                "skjdhcljsdn")
+        newsList.add(news)
+
+        news = News("kjk", "901u3wjridjslkdfskmdnkjsnev", "sjdhcjsd",
+                "skjdhcljsdn")
+        newsList.add(news)
+
+        news = News("mnmxn cmn x", "al;kdpiqwldknalnd", "sjdhcjsd",
+                "skjdhcljsdn")
+        newsList.add(news)
     }
 }
--- app/src/main/java/newsapp/sonia/com/cfnewsapp/NewsAdapter.kt
+++ app/src/main/java/newsapp/sonia/com/cfnewsapp/NewsAdapter.kt
@@ -2,27 +2,31 @@ package newsapp.sonia.com.cfnewsapp
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.news_item.view.*
+
 
 /**
  * Created by soniawadji on 01/06/18.
  */
-class NewsAdapter(private val context: Context) : RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
+class NewsAdapter(private val context: Context, private var newsList: ArrayList<News>) :
+        RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NewsHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.news_item, parent, false)
+        return NewsHolder(view)
     }
 
     override fun onBindViewHolder(holder: NewsHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val news = newsList[position]
+
+        holder?.itemView?.newsTitle?.text = news.title
+        holder?.itemView?.newsDescription?.text = news.description
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = newsList.size
 
-    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
