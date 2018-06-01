3c15ed03084499c83aab21908745d5d82ef1f0f1
Sonia Wadji <soniawadji@gmail.com>
2018-06-01 23:44:51 +0530

    Commit 1.2 Created an adapter class and news Model.. Need to add implementation to adapter class

---
+++ app/src/main/java/newsapp/sonia/com/cfnewsapp/NewsAdapter.kt
@@ -0,0 +1,28 @@
+package newsapp.sonia.com.cfnewsapp
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by soniawadji on 01/06/18.
+ */
+class NewsAdapter(private val context: Context) : RecyclerView.Adapter<NewsAdapter.NewsHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NewsHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NewsHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
